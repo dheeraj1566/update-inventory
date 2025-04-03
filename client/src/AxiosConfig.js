ed95877206b8351de2abf185116ae9e990bc043a
Priyanshiag1812 <priyanshiagarwal34269@iisuniv.ac.in>
2025-04-03 14:12:19 +0530

    Search Inventory 2

--- client/src/AxiosConfig.js
+++ client/src/AxiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Instance = axios.create({
-    // baseURL: `https://inventory-management-8mn8.onrender.com`,
-    baseURL: "http://localhost:8080/",
+    baseURL: `https://inventory-management-system-backend-grnp.onrender.com`,
+    // baseURL: "http://localhost:8080/",
     withCredentials:true,
 });
 
