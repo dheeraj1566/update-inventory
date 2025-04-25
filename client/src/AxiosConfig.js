b6ba4aca4a346488040e4b34a418cf405e5eabd5
dheeraj1566 <dheerajjangid013@gmail.com>
2025-04-25 18:30:06 +0530

    change axios url

--- client/src/AxiosConfig.js
+++ client/src/AxiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Instance = axios.create({
-    // baseURL: `https://update-inventory-eikt.onrender.com`,
-    baseURL: "http://localhost:8080/",
+    baseURL: `https://update-inventory-eikt.onrender.com`,
+    // baseURL: "http://localhost:8080/",
     withCredentials:true,
 });
 
